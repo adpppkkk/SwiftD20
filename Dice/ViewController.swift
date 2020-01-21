4d125bdd383df5e7ac6b9c8c1fc6aefddaedbcd1
sihong Guo <sxg16a@acu.edu>
2020-01-21 13:57:20 -0600

    initial

---
+++ Dice/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by Sihong Guo on 1/21/20.
+//  Copyright © 2020 Sihong Guo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
